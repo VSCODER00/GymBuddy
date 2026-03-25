4bd2fc4de6a97ee8584217b88931ae27ba59184d
vatsal_shah_1290 <vatsalshah1290@gmail.com>
2026-03-26 02:14:36 +0530

    added signup and login logic

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,41 @@
+const userdb=require('../models/user.model')
+const bcrypt = require("bcrypt");
+
+const signup=async(req,res)=>{
+    const {email,password}=req.body
+    if(!email || !password){
+        return res.status(400).json({message:"All the fields are required"})
+    }
+    const user=await userdb.findOne({email})
+    if(user){
+        return res.status(400).json({message:"Already registered"})
+    }
+    const hashedPassword=await bcrypt.hash(password,10)
+    const newDetails = new userdb({
+        email: email,
+        hashed_password: hashedPassword,
+    });
+    await newDetails.save()
+    
+    res.status(201).json({message:"User successfully registered! "})
+}
+
+const login=async(req,res)=>{
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ message: "All the fields are required" });
+    }
+    const user=await userdb.findOne({email})
+    if(!user){
+        return res.status(400).json({message:"Invalid credentials"})
+    }
+    const actualPassword=user.hashed_password
+    const isMatch=await bcrypt.compare(password,actualPassword)
+    if(!isMatch){
+        return res.status(400).json({ message: "Invalid credentials" });
+    }
+    return res.status(200).json({ message: "Signed in successfully" });
+    
+
+}
+module.exports={signup,login}
--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,13 @@ const express=require('express')
 const mongoose=require('mongoose')
 const app=express()
 const mongoDBURL = process.env.MONGODB_URL;
+app.use(express.json());
+const authRoutes=require("./routes/auth.routes")
+
 app.get('/',(req,res)=>{
     res.send("hello world")
 })
-
+app.use('/auth',authRoutes)
 app.listen(5000,()=>{
     console.log("app is running")
 })
