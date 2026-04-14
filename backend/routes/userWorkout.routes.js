b6704ff65cb684f50e9a5565fae24ef91bbca806
vatsal_shah_1290 <vatsalshah1290@gmail.com>
2026-04-15 00:46:51 +0530

    added all user workout routes

--- backend/routes/userWorkout.routes.js
+++ backend/routes/userWorkout.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const isAuthenticated = require("../middleware/auth.middleware");
-const { setUserWorkout } = require("../controllers/userWorkout.controller");
+const { setUserWorkout, getUserWorkout, updateUserWorkout } = require("../controllers/userWorkout.controller");
 
 const router=express.Router()
 
 
 router.get('/setWorkout',isAuthenticated,setUserWorkout)
+router.get('/getWorkout',isAuthenticated,getUserWorkout)
+router.put('/updateWorkout',isAuthenticated,updateUserWorkout)
 module.exports=router
